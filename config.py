cde99c52072eef83d70452a3e74abc0dd9bdc039
Spifon <bateycurukov@gmail.com>
2026-08-27 13:38:21 +0700

    Refactor environment variable assignments in config.py

--- config.py
+++ config.py
@@ -2,13 +2,18 @@ import os
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-PORT = int(os.getenv("PORT", "10000"))
+GROQ_MODEL = os.getenv(
+    "GROQ_MODEL",
+    "openai/gpt-oss-120b"
+)
 
-# Главный пользователь Cho Второго
-OWNER_ID = 5514641516
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
 
-# Модель Groq
-GROQ_MODEL = os.getenv("GROQ_MODEL", "openai/gpt-oss-120b")
+PORT = int(
+    os.getenv("PORT", "10000")
+)
